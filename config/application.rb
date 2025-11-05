a0eb91b9bc6ef5ee6a7589ce5ed46655b81eedcb
Reo05-code <t.asaishi05@gmail.com>
2025-11-05 14:22:52 +0900

    RSpecの初期設定を完了

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,14 @@ module TechLogApp
   class Application < Rails::Application
     config.load_defaults 7.0
 
-    config.generators do |g| # ここから追記
+    config.generators do |g|
       g.assets false          # CSS, JavaScriptファイルを自動生成しない
       g.helper     false      # helperファイルを自動生成しない
+      g.test_framework :rspec, 
+        fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
+        view_specs: false, # ビューファイル用のスペックを作成しない
+        helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+        routing_specs: false # routes.rb用のスペックファイル作成しない
     end
   end
 end
